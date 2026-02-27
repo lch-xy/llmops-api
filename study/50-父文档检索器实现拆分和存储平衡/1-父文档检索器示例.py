986e7993a900f68226e1fff20bce896ba19fffa3
L。 <421558569@qq.com>
2026-02-27 10:40:48 +0800

    Langchain框架学习 子文档检索父文档

---
+++ study/50-父文档检索器实现拆分和存储平衡/1-父文档检索器示例.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time   : 2026/2/26 19:44
+@Author  : LCH
+@File   : 1-父文档检索器示例.py
+"""
+from os import getenv
+
+import dotenv
+import weaviate
+from langchain_classic.retrievers import ParentDocumentRetriever
+from langchain_classic.storage import LocalFileStore
+from langchain_community.document_loaders import UnstructuredFileLoader
+from langchain_ollama import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_weaviate import WeaviateVectorStore
+from weaviate.auth import AuthApiKey
+
+dotenv.load_dotenv()
+
+# 1.创建加载器与文档列表，并加载文档
+loaders = [
+    UnstructuredFileLoader("./电商产品数据.txt"),
+    UnstructuredFileLoader("./项目API文档.md"),
+]
+docs = []
+for loader in loaders:
+    docs.extend(loader.load())
+
+# 2.创建文本分割器
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=50,
+)
+
+# 3.创建向量数据库与文档数据库
+vector_store = WeaviateVectorStore(
+    client=weaviate.connect_to_weaviate_cloud(
+        cluster_url=getenv("WEAVIATE_URL"),
+        auth_credentials=AuthApiKey(getenv("WEAVIATE_API_KEY")),
+    ),
+    index_name="ParentDocument",
+    text_key="text",
+    embedding=OllamaEmbeddings(model="embeddinggemma"),
+)
+byte_store = LocalFileStore("./parent-document")
+
+# 4.创建父文档检索器
+retriever = ParentDocumentRetriever(
+    vectorstore=vector_store,
+    byte_store=byte_store,
+    child_splitter=text_splitter,
+)
+
+# 5.添加文档
+retriever.add_documents(docs, ids=None)
+
+# 6.检索并返回内容
+search_docs = retriever.vectorstore.similarity_search("分享关于LLMOps的一些应用配置")
+# search_docs = retriever.invoke("分享关于LLMOps的一些应用配置")
+print(search_docs)
+print(len(search_docs))
+
+vector_store._client.close()
